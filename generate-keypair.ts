efbb775103c32ab35bc4511c831a0cb3bf21277f
Shradhesh_jain <shradeshjain123@gmail.com>
2024-06-18 00:24:54 +0530

    first commit

---
+++ generate-keypair.ts
@@ -0,0 +1,14 @@
+import { Keypair } from "@solana/web3.js";
+// const keypair = Keypair.generate();
+// console.log("Generated keypair");
+
+
+//                typescript file give me error messages: 
+// console.log("Generated keypair");
+// ^
+
+
+// SyntaxError: Octal escape sequences are not allowed in strict mode.
+//     at ESMLoader.moduleStrategy (node:internal/modules/esm/translators:117:18)
+//     at ESMLoader.moduleProvider (node:internal/modules/esm/loader:463:14)
+//     at async link (node:internal/modules/esm/module_job:68:21)
